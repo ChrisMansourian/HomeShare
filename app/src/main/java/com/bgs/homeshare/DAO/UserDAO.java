d474ceba29b61a5adb963011c58fb938394d530d
Chris Mansourian <cmansour@usc.edu>
2022-10-29 11:28:05 -0700

    Added support for the Personality Questions

--- app/src/main/java/com/bgs/homeshare/DAO/UserDAO.java
+++ app/src/main/java/com/bgs/homeshare/DAO/UserDAO.java
@@ -118,7 +118,8 @@ public class UserDAO {
     }
 
     public static boolean UpdateProfile(String userName, String dob, String email, String number,
-                                     String academicFocus, String schoolYear, String personalIntro, byte[] img) {
+                                     String academicFocus, String schoolYear, String personalIntro, byte[] img,
+                                        String personalityQuestion1, String personalityQuestion2, String personalityQuestion3) {
         Connection c = SqlConnection.GetConnection();
         boolean result = false;
         try {
@@ -131,7 +132,10 @@ public class UserDAO {
                     + " \'" + academicFocus + "\',"
                     + " \'" + schoolYear + "\',"
                     + " \'" + personalIntro + "\',"
-                    + " ?";
+                    + " ?,"
+                    + " \'" + personalityQuestion1 + "\',"
+                    + " \'" + personalityQuestion2 + "\',"
+                    + " \'" + personalityQuestion3 + "\'";
             PreparedStatement stmt = c.prepareStatement(SQL);
             stmt.setDate(1, sqlDate);
             stmt.setBytes(2, img);
@@ -150,7 +154,8 @@ public class UserDAO {
     }
 
     public static boolean CreateAccount(String userName, String password, String dob, String email, String number,
-                                        String academicFocus, String schoolYear, String personalIntro, byte[] img) {
+                                        String academicFocus, String schoolYear, String personalIntro, byte[] img,
+                                        String personalityQuestion1, String personalityQuestion2, String personalityQuestion3) {
         Connection c = SqlConnection.GetConnection();
         boolean result = false;
         try {
@@ -164,7 +169,10 @@ public class UserDAO {
                     + " \'" + academicFocus + "\',"
                     + " \'" + schoolYear + "\',"
                     + " \'" + personalIntro + "\',"
-                    + " ?";
+                    + " ?,"
+                    + " \'" + personalityQuestion1 + "\',"
+                    + " \'" + personalityQuestion2 + "\',"
+                    + " \'" + personalityQuestion3 + "\'";
             PreparedStatement stmt = c.prepareStatement(SQL);
             stmt.setDate(1, sqlDate);
             stmt.setBytes(2, img);
@@ -192,8 +200,11 @@ public class UserDAO {
         String schoolYear = rs.getString("SchoolYear");
         String personalIntroduction = rs.getString("PersonalIntroduction");
         byte[] imgBytes = rs.getBytes("ProfilePicture");
+        String personalityQuestion1 = rs.getString("PersonalityQuestion1");
+        String personalityQuestion2 = rs.getString("PersonalityQuestion2");
+        String personalityQuestion3 = rs.getString("PersonalityQuestion3");
 
-        return new User(uId, uName, DOB, email, phoneNumber, academicFocus, schoolYear, personalIntroduction, imgBytes);
+        return new User(uId, uName, DOB, email, phoneNumber, academicFocus, schoolYear, personalIntroduction, imgBytes, personalityQuestion1, personalityQuestion2, personalityQuestion3);
     }
 
 }
