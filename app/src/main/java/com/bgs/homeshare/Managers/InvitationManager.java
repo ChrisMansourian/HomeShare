5467d40384cf5409435618d9c6b8cbca3a2488fe
Aren Melkonian <arenmelk@usc.edu>
2022-11-02 21:39:59 -0700

    Updated To Have Bedrooms and Bathrooms for Property, sort for getPosts, sign up page, log in page, and few smaller fixes

--- app/src/main/java/com/bgs/homeshare/Managers/InvitationManager.java
+++ app/src/main/java/com/bgs/homeshare/Managers/InvitationManager.java
@@ -12,16 +12,11 @@ public class InvitationManager {
     public static Invitation myInvitation;
 
     public static boolean respondToInvitation(int postId, int userId, int response, List<String> questionResponses){
-        if(InvitationDAO.respondToInvitation(postId, userId, response, questionResponses)){
-            getInvitations(userId);
-            return true;
-        }
-        return false;
+        return InvitationDAO.respondToInvitation(postId, userId, response, questionResponses);
     }
 
     public static boolean manageResponse(int postId, int userIdResponseTo, int userIdOwner, int posterResponse){
         if(InvitationDAO.manageResponse(postId, userIdResponseTo, posterResponse, userIdOwner)){
-            getInvitations(userIdOwner);
             getMyInvitation(userIdOwner);
             return true;
         }
@@ -32,8 +27,14 @@ public class InvitationManager {
         myInvitation = InvitationDAO.getInvitationForPosterFromID(userId);
     }
 
-    public static void getInvitations(int userId){
-        invitations = InvitationDAO.getInvitations(userId);
+    /**
+     * @param userId
+     * @param sortCriteria
+     * @param ascending
+     * SORT criteria values, 'squarefeet','distance','bedrooms','bathrooms'
+     */
+    public static void getInvitations(int userId, String sortCriteria, int ascending){//if 1 ascending , 0 descending
+        invitations = InvitationDAO.getInvitations(userId, sortCriteria, ascending);
     }
 
     public static boolean createAnInvitation(Invitation invitation){
