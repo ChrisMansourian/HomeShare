be7611fa9270f8c89bd46d80b83ad57a57687299
Aren Melkonian <arenmelk@usc.edu>
2022-11-05 10:22:21 -0700

    Skeleton Files for All Pages

--- app/src/main/java/com/bgs/homeshare/ViewAResponseActivity.java
+++ app/src/main/java/com/bgs/homeshare/ViewAResponseActivity.java
@@ -23,19 +23,5 @@ public class ViewAResponseActivity extends AppCompatActivity {
 
         binding = ActivityViewAresponseBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-
-        Toolbar toolbar = binding.toolbar;
-        setSupportActionBar(toolbar);
-        CollapsingToolbarLayout toolBarLayout = binding.toolbarLayout;
-        toolBarLayout.setTitle(getTitle());
-
-        FloatingActionButton fab = binding.fab;
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 }
